0a9f87ae7dd7c86e285eee222d7692f4e72a87ee
Katie Lewis <klewi314@gmail.com>
2018-06-05 17:28:54 -0700

    Updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,11 +3,16 @@ var orm = require('../config/orm.js');
 //Code that calls the orm functions using specific input for the orm
 var burger = {
   selectAll: function(cb){
-    orm.selectAll("burgers", function(res){
-      cb(res)
-    })
+    orm.selectAll(cb)
+    
+  },
+  insertOne: function(burger_name, cb){
+    orm.insertOne(burger_name, cb);
+  },
+  updateOne: function(id, cb){
+    orm.updateOne(id, cb);
   }
-}
+};
 
 //Export file
 module.exports = burger;
