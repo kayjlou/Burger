b851a8e3f9c0c597eec02660a1c01470720ab3e1
Katie Lewis <klewi314@gmail.com>
2018-06-05 17:46:29 -0700

    Issue with adding new burger

--- config/orm.js
+++ config/orm.js
@@ -1,40 +1,36 @@
 //Import & require connection.js into file
 var connection = require('../config/connection.js');
 
-let tableName = "burgers";
+//Define tableName
+var tableName = "burgers";
 
-let orm = {
+orm = {
   // Get all burgers from DB
-  selectAll: function(callback){
-    
+  selectAll: function(callback){ 
     queryString = 'SELECT * FROM ' + tableName;
     connection.query(queryString, function(err, result) {
       callback(result);
 
     });
   },
+
   // Add new burger to db
   insertOne: function(burger_name, callback){
-    // Query 
-    let queryString = "INSERT INTO " + tableName + " (burger_name, devoured) VALUES (?,?)";
+    queryString = "INSERT INTO " + tableName + " (burger_name, devoured) VALUES (?,?)";
+
     // Set default value of false 
-    let burgerDevoured = false;
-    
+    devoured = false;
     connection.query(queryString, [burger_name, devoured], function(err, result) {
       if(err) console.log(err);
-      
       callback(result);
-
     });
   },
+
   // Update a burger
   updateOne: function(id, callback){
-    
-    let queryString = "UPDATE " + tableName + " SET devoured=1 WHERE id=?";
-
+    queryString = "UPDATE " + tableName + " SET devoured=1 WHERE id=?";
     connection.query(queryString, [id], function(err, result) {
       if(err) console.log(err);
-
       callback(result);
 
     });
